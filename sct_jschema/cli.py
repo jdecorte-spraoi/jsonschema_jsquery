60b23e4e3c9245757c7e52afb365dc6eb05824c6
Jason DeCorte <jdecorte@decorteindustries.com>
2020-01-02 21:41:07 -0500

    working CLI

--- sct_jschema/cli.py
+++ sct_jschema/cli.py
@@ -1,7 +1,33 @@
+import json
+
 import click
 
 
+def get_type_for_key_path(schema: dict, key_path: str) -> [str, None]:
+    """
+    Get the type for the given key path in the schema
+    """
+    properties = schema.get("properties")
+    keys = key_path.split(".")
+
+    for key in keys:
+        if key not in properties:
+            return None
+        if "type" in properties[key]:
+            return properties[key]['type']
+        elif "$ref" in properties[key]:
+            definition_key = properties[key]['$ref'].split("/")[-1]
+            properties = schema.get("definitions").get(definition_key).get('properties')
+
+    return None
+
+
 @click.command()
-def main():
-    """ test """
-    click.echo("My first CLI")
+@click.argument("schema_file", type=click.File("rb"))
+@click.argument("key_path")
+def main(schema_file: click.File, key_path: str):
+    """ Given a valid JSON Schema and a valid key path, return the type value """
+    schema_dict = json.load(schema_file)
+    click.echo(f"Schema File = {schema_file.name}")
+    click.echo(f"Key Path    = {key_path}")
+    click.echo(f"Type        = {get_type_for_key_path(schema=schema_dict, key_path=key_path)}")
